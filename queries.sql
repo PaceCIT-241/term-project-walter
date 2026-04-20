1776f27a8f281f7fc5991423b8122689aff9aacd
Walter <Wd11357p@pace.edu>
2026-04-20 18:40:33 -0400

    Final fixes

--- queries.sql
+++ queries.sql
@@ -1,12 +1,9 @@
--- Top 100 movies
 SELECT * FROM Movies LIMIT 100;
 
--- Join Movies and Genre
 SELECT M.Title, G.GenreName
 FROM Movies M
 JOIN Genre G ON M.GenreID = G.GenreID
 LIMIT 100;
 
--- Average rating
 SELECT AVG(Rating) AS AvgRating
 FROM Ratings;
